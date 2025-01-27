6a477798333adb4de35821fe9ec61261d2e8f2a6
rehan-abbas <rehanhello20@gmail.com>
2025-01-27 17:58:29 +0530

    updated minor errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
   },
   build: {
     outDir: "dist",
