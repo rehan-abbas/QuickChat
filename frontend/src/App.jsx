3ec7d5e8a5ce539f710258b853461286e8fc0481
rehan-abbas <rehanhello20@gmail.com>
2025-01-28 17:20:49 +0530

    updated minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   if (isCheckingAuth && !authUser)
     return (
       <div className="flex items-center justify-center h-screen">
-        <Loader className="siz-10 animate-spin" />
+        <Loader className="size-10 animate-spin" />
       </div>
     );
   return (
